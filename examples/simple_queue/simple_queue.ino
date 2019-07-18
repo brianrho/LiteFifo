87e08b3efd753b1ef71f4eba8d7450295a955553
Brian Ejike <brianrho94@gmail.com>
2019-07-18 02:38:22 +0100

    Update example

--- examples/simple_queue/simple_queue.ino
+++ examples/simple_queue/simple_queue.ino
@@ -1,7 +1,15 @@
 #include <lite_fifo.h>
 
 /* user defined struct to be inserted into queue */
+typedef struct {
+    uint16_t x;
+    uint16_t y;
+} Point;
 
+/* backing storage for the queue */
+Point buffer[32];
+/* create FIFO and point to the storage */
+LiteFifo fifo(buffer, 32, sizeof(Point));
 
 void setup(void) {
     Serial.begin(9600);
